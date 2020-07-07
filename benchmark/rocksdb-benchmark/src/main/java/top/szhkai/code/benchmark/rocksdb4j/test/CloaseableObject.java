3db935b40ef6ddcb358444b9f663556dbb16e7cc
bovenson <szhkai@qq.com>
2020-07-07 14:14:08 +0800

    add rocksdb

---
+++ benchmark/rocksdb-benchmark/src/main/java/top/szhkai/code/benchmark/rocksdb4j/test/CloaseableObject.java
@@ -0,0 +1,4 @@
+package top.szhkai.code.benchmark.rocksdb4j.test;
+
+public class CloaseableObject {
+}
---
+++ benchmark/rocksdb-benchmark/src/main/java/top/szhkai/code/benchmark/rocksdb4j/test/TestTryWithResource.java
@@ -0,0 +1,6 @@
+package top.szhkai.code.benchmark.rocksdb4j.test;
+
+public class TestTryWithResource {
+    public static void main(String[] args) {
+    }
+}
