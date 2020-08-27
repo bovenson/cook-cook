e39153573360f7e38d7430bc632f539e12f74f9e
bovenson <szhkai@qq.com>
2020-08-27 16:38:30 +0800

    add json bench mark

--- java/cook-java/cook-sample/src/main/java/pub/wii/cook/java/test/TestA.java
+++ java/cook-java/cook-sample/src/main/java/pub/wii/cook/java/test/TestA.java
@@ -3,9 +3,12 @@ package pub.wii.cook.java.test;
 public class TestA {
     public static void main(String[] args) {
         String hello = "Hello World";
-        System.out.println(hello.substring(0, hello.lastIndexOf("Wrld")));
+        // System.out.println(hello.substring(0, hello.lastIndexOf("Wrld")));
 
         // Number number = Double.NaN;
         // System.out.println(number);
+        String s = "HELLO";
+        System.out.println(String.valueOf(s));
+        System.out.println(s.toString());
     }
 }
