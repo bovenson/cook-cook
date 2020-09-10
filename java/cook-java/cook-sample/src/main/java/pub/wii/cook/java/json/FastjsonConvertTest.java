965dcc8b5562d2050510d754badb49c6f9e30778
bovenson <szhkai@qq.com>
2020-09-10 20:17:28 +0800

    add node

--- java/cook-java/cook-sample/src/main/java/pub/wii/cook/java/json/FastjsonConvertTest.java
+++ java/cook-java/cook-sample/src/main/java/pub/wii/cook/java/json/FastjsonConvertTest.java
@@ -22,7 +22,5 @@ public class FastjsonConvertTest {
     }
 
     public static void main(String[] args) {
-        String s = "{\"adapt_id\":\"\",\"adapt_self\":6672130,\"app_id\":1,\"area_id\":4,\"category_description\":\"\",\"category_name\":\"行動電源\",\"comment_num\":0,\"commodity_id\":317501,\"commodity_name\":\"小米行動電源2C 20000mAh 白色\",\"commodity_short_name\":\"小米行動電源2C 20000mAh 白色\",\"commodity_sort\":0,\"description\":\"\",\"has_store\":0,\"is_batched\":0,\"is_discount\":0,\"is_new\":0,is_real\":1,\"is_sale\":1,\"keywords\":\"行動電源,小米行動電源2C20000mAh\",\"last_index_time\":1599479776,\"market_price\":159,\"pattrvalue_1\":\"\",\"pattrvalue_2\":\"\",\"pattrvalue_3\":\"\",\"pattrvalue_4\":\"\",\"pattrvalprice\":159,\"product_description\":\"\",\"product_id\":3353,\"product_name\":\"小米行動電源2C 20000mAh\",\"product_short_name\":\"小米行動電源2C 20000mAh\",\"product_sort\":0,\"search_score\":0,\"search_top\":0,\"spec_value_id\":\"2\",\"update_time\":1535946707,\"add_time\":1513679993,\"adapt_ids\":null,\"category_id\":69,\"id\":0,\"brand_id\":0}";
-        // JSON
     }
 }
---
+++ nodejs/cook-nodejs/cook-font-carrier/font.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const fontCarrier = require('font-carrier');
+const font = fontCarrier.create();
+// font.setSvg('test', fs.readFileSync('test.svg').toString());
+font.setGlyph('1', {
+    glyphName: '1',
+    horizAdvX: '1024',
+    svg: fs.readFileSync('src/1.svg').toString()
+});
+
+font.setGlyph('2', {
+    glyphName: '2',
+    horizAdvX: '1024',
+    svg: fs.readFileSync('src/2.svg').toString()
+});
+
+font.setGlyph('test', {
+    glyphName: 'test',
+    horizAdvX: '1024',
+    svg: fs.readFileSync('src/test.svg').toString()
+});
+
+// var svg = font.getSvg('test');
+font.output({
+    path: './output/test'
+});
