44cf2821c20478e86feb62dcd0e950253dca4a8d
bovenson <szhkai@qq.com>
2020-10-12 21:23:22 +0800

    stage

---
+++ java/cook-java/cook-spring-cloud-alibaba-provider/src/main/java/pub/wii/cook/provider/UserServiceProviderBootstrap.java
@@ -0,0 +1,9 @@
+package pub.wii.cook.provider;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableDiscoveryClient
+@EnableAutoConfiguration
+public class UserServiceProviderBootstrap {
+
+}
