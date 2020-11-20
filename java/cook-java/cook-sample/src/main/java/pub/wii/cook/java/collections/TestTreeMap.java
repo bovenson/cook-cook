497f7d8d32a492ab4819ee3bb61aaf60c761e322
bovenson <szhkai@qq.com>
2020-11-20 12:12:19 +0800

    add ml

--- java/cook-java/cook-sample/src/main/java/pub/wii/cook/java/collections/TestTreeMap.java
+++ java/cook-java/cook-sample/src/main/java/pub/wii/cook/java/collections/TestTreeMap.java
@@ -1,35 +1,16 @@
 package pub.wii.cook.java.collections;
 
-import org.apache.commons.lang3.tuple.Pair;
-
-import java.util.TreeSet;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class TestTreeMap {
     public static void main(String[] args) {
-        TreeSet<Pair<Integer, Integer>> ts = new TreeSet<>(
-                (o1, o2) -> o2.getRight().equals(o1.getRight())
-                        ? o1.getLeft() - o2.getLeft()
-                        : o1.getRight() - o2.getRight()
-        );
-        ts.add(Pair.of(2, 2));
-        ts.add(Pair.of(1, 2));
-        ts.add(Pair.of(4, 0));
-        ts.add(Pair.of(3, 2));
-        ts.add(Pair.of(5, 1));
-        System.out.println(ts);
-        Pair<Integer, Integer> poll = ts.pollFirst();
-        System.out.println(ts);
-        assert poll != null;
-        System.out.println(poll);
-        ts.add(Pair.of(poll.getLeft(), poll.getRight() + 10));
-        System.out.println(ts);
-
-        System.out.println();
-        poll = ts.pollFirst();
-        System.out.println(ts);
-        assert poll != null;
-        System.out.println(poll);
-        ts.add(Pair.of(poll.getLeft(), poll.getRight() + 2));
-        System.out.println(ts);
+        Map<Integer, Integer> tm = new TreeMap<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return 0;
+            }
+        });
     }
 }
